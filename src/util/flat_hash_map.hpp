56757d718291f5bf593fa7634557c61816560ecb
bakjedev <bakje.mail@gmail.com>
2026-06-08 17:28:50 +0200

    flat_hash_map changes

--- src/util/flat_hash_map.hpp
+++ src/util/flat_hash_map.hpp
@@ -11,22 +11,25 @@ namespace fwrk {
     static constexpr uint8_t STATE_DELETED = 0xFF; // 0b1111'1111
     static constexpr uint8_t STATE_OCCUPIED = 0x00; // 0b0000'0000
 
-    using value_type = std::pair<const Key, Value>;
+    using element_type = std::pair<const Key, Value>;
+    using mut_element_type = std::pair<Key, Value>;
 
     struct bucket {
       uint8_t state = STATE_EMPTY;
       union {
-        value_type slot_;
+        mut_element_type mut_elem;
+        element_type elem;
       };
 
       bucket() {}
       ~bucket() {}
 
-      value_type& slot() { return slot_; }
-      const value_type& slot() const { return slot_; }
-      const Key& key() const { return slot_.first; }
-      Value& value() { return slot_.second; }
-      const Value& value() const { return slot_.second; }
+      element_type& element() { return elem; }
+      const element_type& element() const { return elem; }
+
+      const Key& key() const { return elem.first; }
+      Value& value() { return mut_elem.second; }
+      const Value& value() const { return elem.second; }
     };
 
     std::vector<bucket> buckets_;
@@ -39,8 +42,8 @@ namespace fwrk {
 
     template<bool Const>
     class iterator_impl {
-      using value_ref = std::conditional_t<Const, const value_type&, value_type&>;
-      using value_ptr = std::conditional_t<Const, const value_type*, value_type*>;
+      using value_ref = std::conditional_t<Const, const element_type&, element_type&>;
+      using value_ptr = std::conditional_t<Const, const element_type*, element_type*>;
       using vector_ptr = std::conditional_t<Const, const std::vector<bucket>*, std::vector<bucket>*>;
 
       vector_ptr buckets_;
@@ -54,7 +57,7 @@ namespace fwrk {
       }
 
     public:
-      value_ref operator*() const { return (*buckets_)[index_].slot(); }
+      value_ref operator*() const { return (*buckets_)[index_].element(); }
       value_ptr operator->() const { return &**this; }
 
       iterator_impl& operator++()
@@ -86,11 +89,11 @@ namespace fwrk {
       buckets_.resize(bucket_count_);
 
       for (size_t i = 0; i < bucket_count_; i++) {
-        const bucket& other_slot = other.buckets_[i];
-        if ((other_slot.state & STATE_EMPTY) == 0) {
-          bucket& slot = buckets_[i];
-          new (&slot.slot_) value_type(other_slot.slot_);
-          slot.state = other_slot.state;
+        const bucket& other_elem = other.buckets_[i];
+        bucket& elem = buckets_[i];
+        elem.state = other_elem.state;
+        if ((other_elem.state & STATE_EMPTY) == 0) {
+          new (&elem.mut_elem) mut_element_type(other_elem.mut_elem);
         }
       }
     }
@@ -167,12 +170,12 @@ namespace fwrk {
       size_t index = get_index(hash);
 
       while (true) {
-        const bucket* slot = &buckets_[index];
-        if ((slot->state & STATE_EMPTY) == 0) {
-          if (slot->key() == key) {
+        const bucket* elem = &buckets_[index];
+        if ((elem->state & STATE_EMPTY) == 0) {
+          if (elem->key() == key) {
             return const_iterator(&buckets_, index, bucket_count_);
           }
-        } else if (slot->state == STATE_EMPTY) {
+        } else if (elem->state == STATE_EMPTY) {
           return end();
         }
 
@@ -190,10 +193,10 @@ namespace fwrk {
 
     iterator erase(iterator pos)
     {
-      bucket& slot = buckets_[pos.index_];
+      bucket& elem = buckets_[pos.index_];
 
-      slot.state = STATE_DELETED;
-      slot.slot_.~pair();
+      elem.state = STATE_DELETED;
+      elem.mut_elem.~pair();
 
       element_count_--;
 
@@ -203,10 +206,10 @@ namespace fwrk {
 
     void clear()
     {
-      for (bucket& slot: buckets_) {
-        if ((slot.state & STATE_EMPTY) == 0) {
-          slot.slot_.~pair();
-          slot.state = STATE_EMPTY;
+      for (bucket& elem: buckets_) {
+        if ((elem.state & STATE_EMPTY) == 0) {
+          elem.mut_elem.~pair();
+          elem.state = STATE_EMPTY;
         }
       }
 
@@ -272,21 +275,21 @@ namespace fwrk {
       const size_t new_index_mask = new_bucket_count - 1;
 
       for (size_t i = 0; i < bucket_count_; i++) {
-        bucket& slot = buckets_[i];
-        if ((slot.state & STATE_EMPTY) == 0) {
-          const size_t hash = get_hash(slot.key());
+        bucket& elem = buckets_[i];
+        if ((elem.state & STATE_EMPTY) == 0) {
+          const size_t hash = get_hash(elem.key());
           size_t new_index = hash & new_index_mask;
-          bucket* new_bucket = &new_buckets[new_index];
+          bucket* new_elem = &new_buckets[new_index];
 
-          while (new_bucket->state != STATE_EMPTY) {
+          while (new_elem->state != STATE_EMPTY) {
             new_index = (new_index + 1) & new_index_mask;
-            new_bucket = &new_buckets[new_index];
+            new_elem = &new_buckets[new_index];
           }
 
-          new (&new_bucket->slot_) value_type(std::move(const_cast<Key&>(slot.key())), std::move(slot.value()));
-          new_bucket->state = STATE_OCCUPIED;
+          new (&new_elem->mut_elem) mut_element_type(std::move(elem.mut_elem.first), std::move(elem.mut_elem.second));
+          new_elem->state = STATE_OCCUPIED;
 
-          slot.slot_.~pair();
+          elem.mut_elem.~pair();
         }
       }
 
@@ -313,13 +316,13 @@ namespace fwrk {
       std::optional<size_t> first_deleted;
 
       while (true) {
-        bucket* slot = &buckets_[index];
+        bucket* elem = &buckets_[index];
 
-        if ((slot->state & STATE_EMPTY) == 0) {
-          if (slot->key() == key) {
+        if ((elem->state & STATE_EMPTY) == 0) {
+          if (elem->key() == key) {
             return {index, true};
           }
-        } else if (slot->state == STATE_DELETED) {
+        } else if (elem->state == STATE_DELETED) {
           if (!first_deleted) {
             first_deleted = index;
           }
@@ -333,10 +336,10 @@ namespace fwrk {
     template<typename K, typename... Args>
     iterator construct_at(const size_t index, K&& key, Args&&... args)
     {
-      bucket& slot = buckets_[index];
-      new (&slot.slot_) value_type(std::piecewise_construct, std::forward_as_tuple(std::forward<K>(key)),
-                                   std::forward_as_tuple(std::forward<Args>(args)...));
-      slot.state = STATE_OCCUPIED;
+      bucket& elem = buckets_[index];
+      new (&elem.mut_elem) mut_element_type(std::piecewise_construct, std::forward_as_tuple(std::forward<K>(key)),
+                                            std::forward_as_tuple(std::forward<Args>(args)...));
+      elem.state = STATE_OCCUPIED;
       element_count_++;
       occupied_count_++;
       return iterator(&buckets_, index, bucket_count_);
